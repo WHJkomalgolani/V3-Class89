70b5c15791ef8e5959f92305173794cf634ed9c3
WHJkomalgolani <68747415+WHJkomalgolani@users.noreply.github.com>
2021-06-16 13:58:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDbIg4SRD9FDd3AQjUoUNsRmvTSo4xEfFQ',
+  authDomain: 'demostory-9d7ca.firebaseapp.com',
+  databaseURL:
+    'https://demostory-9d7ca-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'demostory-9d7ca',
+  storageBucket: 'demostory-9d7ca.appspot.com',
+  messagingSenderId: '641679640485',
+  appId: '1:641679640485:web:8e1b274bab4bda55058a4c',
+};
